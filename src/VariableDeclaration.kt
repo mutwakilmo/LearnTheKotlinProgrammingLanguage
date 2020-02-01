312432b9fe63fccbc9a56fab31d07471a84b95b4
kingkashta <mutwakil.m.osman@gmail.com>
2020-02-01 22:47:18 +0100

    Variable declaration

---
+++ src/VariableDeclaration.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    //Use var for a variable whose value can change
+    var count : Int = 10
+    //change value of count
+    count = 15
+
+
+    //Use val variable value never chages
+    val languageName : String = "Kotlin"
+}
